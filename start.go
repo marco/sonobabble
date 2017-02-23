6ecc717c71f6a76c3cb455fcc6909dddb91ddc45
Skunk <skunkmbtech@gmail.com>
2017-02-22 16:58:17 -0800

    Completely replace start.go with githubstart.go
    Having two main files is confusing, and causes an error when using go
    get which states that the repository has two main packages. Remove the
    comment from the top of githubstart.go, and rename it to start.go while
    removing start.go in the process.

--- start.go
+++ start.go
@@ -1,12 +1,6 @@
-/*
-	NOTE: This file is intended for use inside of $GOPATH/src/sonobabble (IE not the default go get location when
-	downloaded from GitHub), because it imports from sonobabble/sonobabble. If you are running Sonobabble inside
-	of $GOPATH/src/github.com/skunkmb/sonobabble, please use githubstart.go instead.
-*/
-
 package main
 
-import "sonobabble/sonobabble"
+import "github.com/skunkmb/sonobabble/sonobabble"
 
 // main will get the ball rolling with Sonobabble by calling the Serve function of the sonobabble package.
 func main() {
